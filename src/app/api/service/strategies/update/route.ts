e2d2cf1169156aa9a1d7ed890a891833bde583a6
Nemurenai <vlad95700@gmail.com>
2024-12-03 00:11:27 +0200

    fix strategies & other

---
+++ src/app/api/service/strategies/update/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server"
+
+import { logger } from "@/lib/server/providers/logger"
+import { strategiesService } from "@/lib/server/services/strategies.service"
+import { tokensService } from "@/lib/server/services/tokens.service"
+
+// endpoint required for instrumentation.ts
+export const POST = async (req: NextRequest) => {
+    const { accessToken } = await req.json()
+    try {
+        await tokensService.verifyServiceAccessToken(accessToken)
+    } catch (error) {
+        logger.warn(error, "Service Access Token error")
+        return NextResponse.json({ error: "Not Authorized" }, { status: 401 })
+    }
+
+    try {
+        await strategiesService.checkAndClose()
+        await strategiesService.calcPnlForAll()
+
+        return NextResponse.json({}, { status: 200 })
+    } catch (error) {
+        logger.error(error)
+        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 })
+    }
+}
