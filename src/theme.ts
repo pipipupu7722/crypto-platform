1c50d83b1302be5d83896d7fff5285d3ead8370a
Ignat Popov <glebzubov@Noutbuk-Gleb.local>
2024-12-11 02:30:50 +0200

    fix custom color tokens and flexible theme typing

--- src/theme.ts
+++ src/theme.ts
@@ -1,19 +1,27 @@
+// src/theme.ts
+
 import { theme } from "antd"
+
+type CustomToken = {
+    colorBgHeader: string,
+    colorBgHeaderButton: string,
+} & {
+    [key: string]: any;  
+}
 
 export const themeConfig = {
     algorithm: theme.defaultAlgorithm,
     token: {
+        ...theme.defaultConfig?.token,
         fontFamily: "IBM Plex Sans",
-
         colorPrimary: "#F7A600",
         colorLink: "#F7A600",
-
         colorBgSolid: "#111111",
         colorBgLayout: "#F5F7FA",
         colorBgSider: "#2C2E30",
-        colorBgHeader: "#17181E",
-        colorBgHeaderButton: "#121214",
-    },
+        colorBgHeader: "#17181E", 
+        colorBgHeaderButton: "#121214", 
+    } as CustomToken, 
 }
 
 export const breakpoints = {
