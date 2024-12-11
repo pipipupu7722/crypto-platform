0f9f771858ce5939c5486c7266a47d14c5573433
Ignat Popov <glebzubov@Noutbuk-Gleb.local>
2024-12-11 13:26:40 +0200

    changed font Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -25,6 +25,7 @@ export default function Header({ children }: PropsWithChildren) {
                 display: flex;
                 justify-content: space-around;
                 background-color: #1b1c1c;
+                fontFamily: "IBM Plex Sans";
             `}
         >
             <div
